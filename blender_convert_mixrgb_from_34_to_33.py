08c961b72f427c91bc80900bb40689197940ca52
zebus3d <48990185+zebus3d@users.noreply.github.com>
2022-12-14 13:44:39 +0100

    recursive

--- blender_convert_mixrgb_from_34_to_33.py
+++ blender_convert_mixrgb_from_34_to_33.py
@@ -1,27 +1,31 @@
 import bpy
+from bpy.types import ShaderNodeTree
 
 # This script is for convert MixRGB nodes from Blender 3.4 to MixRGB nodes to Blender 3.3, because 3.4 are incompatibles in 3.3.
 # Run this script in Blender 3.3
 
-for obj in bpy.data.objects:
-    if obj.type != 'MESH':
-        continue
-    #    
-    for mslot in obj.material_slots: 
-        material = mslot.material
-        #   
-        for main_node in material.node_tree.nodes:
-            if main_node.type != 'GROUP': # in my case are in groups in the main
+def process_nodes(root_nodes):
+    # print('root_nodes', root_nodes)
+    #
+    if isinstance(root_nodes, ShaderNodeTree):
+        #
+        nodes = root_nodes.nodes
+        #
+        for node in nodes:
+            if node.type == 'FRAME':
                 continue
-            nodes = main_node.node_tree.nodes
             #
-            for node in nodes:
+            if node.type == 'GROUP':
+                if hasattr(node, "node_tree"):
+                    process_nodes(node.node_tree)
+            else:
                 if node.type == '':
+                    # print('root_nodes, nodes, node.name', root_nodes, nodes, node.name)
                     #
                     #print('Node name:', node.name)
                     new_node = nodes.new('ShaderNodeMixRGB')
                     new_node.location = node.location.copy()
-                    #new_node.dimensions = node.dimensions (read only)
+                    # new_node.dimensions = node.dimensions (read only)
                     #
                     # connecting the inputs to the new nodes:
                     for i in range(len(node.inputs)):
@@ -45,16 +49,16 @@ for obj in bpy.data.objects:
                             #
                             #print(i, inputx.name)
                             #
-                            left_node = inputx.links[0].from_node
+                            # left_node = inputx.links[0].from_node
                             left_output = inputx.links[0].from_socket
                             #print("left_node:", left_node.name)
-                            #print(left_output)
+                            # print(left_output)
                             if new_factor_input:
-                                main_node.node_tree.links.new(new_node.inputs[new_factor_input], left_output)
+                                root_nodes.links.new(new_node.inputs[new_factor_input], left_output)
                             if new_color1_input:
-                                main_node.node_tree.links.new(new_node.inputs[new_color1_input], left_output)
+                                root_nodes.links.new(new_node.inputs[new_color1_input], left_output)
                             if new_color2_input:
-                                main_node.node_tree.links.new(new_node.inputs[new_color2_input], left_output)
+                                root_nodes.links.new(new_node.inputs[new_color2_input], left_output)
                     # connecting outputs to the new nodes:
                     for i in range(len(node.outputs)):
                         outputx = node.outputs[i]
@@ -70,9 +74,21 @@ for obj in bpy.data.objects:
                             #print("right_node_input:", right_node_input)
                             if new_factor_output:
                                 print("new_factor_output", new_factor_output)
-                                main_node.node_tree.links.new(new_node.outputs[new_factor_output], right_node_input)
+                                root_nodes.links.new(new_node.outputs[new_factor_output], right_node_input)
                     #
                     new_node.name = node.name
                     new_node.select = False
-                    main_node.node_tree.nodes.remove(node)
+                    nodes.remove(node)
+    #
+    # print("\n")
 
+
+for obj in bpy.data.objects:
+    if obj.type != 'MESH' and 'Domain' not in obj:
+        continue
+    #
+    for mslot in obj.material_slots:
+        material = mslot.material
+        #
+        root_nodes = material.node_tree
+        process_nodes(root_nodes)
